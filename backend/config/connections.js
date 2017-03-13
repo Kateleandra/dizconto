edf6c99e9aae8c46846888bb3a7bc2e6cc7bf87b
André Pontes <andrepontes@gmail.com>
2017-03-13 07:59:30 -0300

    backend: persistência dos leads em mongodb.

--- backend/config/connections.js
+++ backend/config/connections.js
@@ -27,10 +27,10 @@ module.exports.connections = {
   *                                                                          *
   * Installed by default.                                                    *
   *                                                                          *
-  ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  ***************************************************************************
+  * localDiskDb: {
+  *    adapter: 'sails-disk'
+  * },
 
   /***************************************************************************
   *                                                                          *
@@ -40,14 +40,15 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
-
+/*
+   mysql: {
+     adapter: 'sails-mysql',
+     host: '127.0.0.1',
+     user: 'root', //optional
+     password: '', //optional
+     database: '' //optional
+   },
+*/
   /***************************************************************************
   *                                                                          *
   * MongoDB is the leading NoSQL database.                                   *
@@ -56,14 +57,11 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+   mongodb: {
+     adapter: 'sails-mongo',
+     host: 'localhost',
+     port: 27017
+   }
 
   /***************************************************************************
   *                                                                          *
