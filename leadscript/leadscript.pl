f03cac8e24d3bca6c37e0e48aacf50c8d1397ce6
André Pontes <andrepontes@gmail.com>
2017-03-15 14:39:01 -0300

    leadscript: acesso ao mongodb

--- leadscript/leadscript.pl
+++ leadscript/leadscript.pl
@@ -5,6 +5,9 @@ use warnings;
 
 use Parse::AccessLog;
 use Data::Dumper;
+use MongoDB;
+
+my %hash_ip;
 
 my $num_args = scalar @ARGV;
 if ( $num_args != 1 ) {
@@ -17,7 +20,26 @@ my @recs = $p->parse( $ARGV[0] );
 
 for my $rec (@recs) {
     my %hash = %{$rec};
-    my $time = $hash{"time_local"};
+    my $time = substr( $hash{"time_local"}, 12, 8 );
     my $ip   = $hash{"remote_addr"};
-    print "$time | $ip\n";
+    $hash_ip{$time} = $ip;
+
+    #    print "$time | $ip\n";
+}
+
+my $client = MongoDB->connect('mongodb://localhost');
+my $collection = $client->ns('sails.lead');    # database foo, collection bar
+
+#my $result     = $collection->insert_one({ some => 'data' });
+my $leads = $collection->find;
+
+while ( my $doc = $leads->next ) {
+    my $time = substr( $doc->{'createdAt'}, 11, 8 );
+    if ( exists $hash_ip{$time} ) {
+        my $ip = $hash_ip{$time};
+        print $time. " $ip \n";
+    }
+    else {
+        print " Não existe na hash \n";
+    }
 }
